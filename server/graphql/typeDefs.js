9d7ff8b4b5eb518bd4fe20d95abc6aa5986b3723
Minh Long <haminhlong3@gmail.com>
2021-01-21 17:16:39 +0000

    Register and Login Users

---
+++ server/graphql/typeDefs.js
@@ -0,0 +1,36 @@
+const { gql } = require("apollo-server");
+
+module.exports = gql`
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    tags: [String!]
+    selectedFile: String!
+    createAt: String!
+    username: String!
+  }
+  type User {
+    id: ID!
+    email: String!
+    username: String!
+    fullName: String!
+    token: String!
+    createdAt: String!
+    # selectedFile: String!
+  }
+  input RegisterInput {
+    email: String!
+    fullName: String!
+    username: String!
+    password: String!
+    confirmPassword: String!
+  }
+  type Query {
+    getPosts: [Post]
+  }
+  type Mutation {
+    register(registerInput: RegisterInput): User!
+    login(username: String!, password: String!): User!
+  }
+`;
