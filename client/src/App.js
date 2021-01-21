96981d52029617b93352aed6178dfdf92b00f5c0
Minh Long <haminhlong3@gmail.com>
2021-01-21 23:54:06 +0000

    Add Routes and Styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Header from "./components/Layouts/Header";
+import Footer from "./components/Layouts/Footer";
+import SignUp from "./components/User/SignUp";
+import Login from "./components/User/Login";
+import Posts from "./components/Posts/Posts";
+import CreatePost from "./components/Posts/Post/CreatePost";
+import Profile from "./components/User/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Router>
+        <Header />
+        <Route path="/" exact component={Posts} />
+        <Route path="/create-post" component={CreatePost} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+        <Footer />
+      </Router>
     </div>
   );
 }
