8c6f9495256e403374a7a022a5c1252c18ee1b0a
Minh Long <haminhlong3@gmail.com>
2021-01-26 17:58:04 +0000

    like button

---
+++ client/src/components/util/graphql.js
@@ -0,0 +1,115 @@
+import gql from "graphql-tag";
+
+export const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      title
+      content
+      tags
+      selectedFile
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      id
+      email
+      fullName
+      username
+      createdAt
+      token
+    }
+  }
+`;
+
+export const SIGN_UP_USER = gql`
+  mutation register(
+    $email: String!
+    $fullName: String!
+    $username: String!
+    $password: String!
+    $confirmPassword: String!
+  ) {
+    register(
+      registerInput: {
+        email: $email
+        fullName: $fullName
+        username: $username
+        password: $password
+        confirmPassword: $confirmPassword
+      }
+    ) {
+      id
+      email
+      fullName
+      username
+      createdAt
+      token
+    }
+  }
+`;
+
+export const CREATE_POST = gql`
+  mutation createPost(
+    $title: String!
+    $content: String!
+    $tags: [String]!
+    $selectedFile: String!
+  ) {
+    createPost(
+      title: $title
+      content: $content
+      tags: $tags
+      selectedFile: $selectedFile
+    ) {
+      id
+      title
+      content
+      tags
+      selectedFile
+      username
+      likes {
+        id
+        username
+        createdAt
+      }
+      likeCount
+      comments {
+        id
+        body
+        username
+        createdAt
+      }
+      commentCount
+    }
+  }
+`;
+
+export const LIKE_POST = gql`
+  mutation likePost($postId: ID!) {
+    likePost(postId: $postId) {
+      id
+      likes {
+        id
+        username
+      }
+      likeCount
+    }
+  }
+`;
